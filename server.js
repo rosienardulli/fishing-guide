9b3bc51ecefc46e03054cccecd1037179d6fa10f
Rosie Nardulli <rosie.nardulli@gmail.com>
2018-07-25 19:10:45 -0500

    login

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Requiring our models for syncing
-var db = require("./models");
+
 
 // Sets up the Express app to handle data parsing
 
@@ -21,14 +21,14 @@ app.use(express.static("public"));
 
 // Routes
 // =============================================================
-require("./routes/api-routes")(app);
+
 require("./routes/html-routes")(app);
 
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ }).then(function() {
+//db.sequelize.sync({ }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
-});
+//});
